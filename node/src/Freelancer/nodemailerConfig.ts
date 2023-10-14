0d78ddfc6f53cab019736d26f353c3ca6a14c49d
Mustapha Talbi <mustapha.talbi2002@gmail.com>
2023-10-14 16:54:33 +0100

    Nodemailer Adjustments

--- node/src/Freelancer/nodemailerConfig.ts
+++ node/src/Freelancer/nodemailerConfig.ts
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 const user = "tuniwork40@gmail.com"; // hedhi t7ot feha l email
 const pass = "rluo ouxk anej fkvt";
 
+// transporter set up (Mustapha)
 const transport = nodemailer.createTransport({
   service: "Gmail",
   auth: {
@@ -10,6 +11,7 @@ const transport = nodemailer.createTransport({
   },
 });
 
+// send freelancer a mail upon sign up (Mustapha)
 export const SendFreelancerAccountConfirmationMail = async (
   name: String,
   email: any,
@@ -25,7 +27,7 @@ export const SendFreelancerAccountConfirmationMail = async (
       html: `
       <div style="font-family: Arial, sans-serif; font-size: 16px;">
   <h1>TuniWork Freelance Account Activation</h1>
-  <img src="https://firebasestorage.googleapis.com/v0/b/tunibids.appspot.com/o/2.jfif?alt=media&token=558a71e4-d312-435b-85ba-cfbf7572766c&_gl=1*17tne49*_ga*MTU1NzQ5MzYxMC4xNjk2NjgwMjU2*_ga_CW55HF8NVT*MTY5NzI4NjI4MS43LjEuMTY5NzI4NjMwNC4zNy4wLjA." style="max-width: 600px; max-height: 320px;">
+  <img src="https://firebasestorage.googleapis.com/v0/b/tunibids.appspot.com/o/OIG.png?alt=media&token=d7e717cb-e3a2-4faa-9f4f-27d4bc4cdf6c&_gl=1*1wd4x8o*_ga*MTU1NzQ5MzYxMC4xNjk2NjgwMjU2*_ga_CW55HF8NVT*MTY5NzI5NzQ0Ny44LjEuMTY5NzI5ODgwMS4yOC4wLjA." style="max-width: 600px; max-height: 320px;">
   <h3>Welcome to TuniWork ! To activate your account, please click the button below.</h3>
   <a href="" style="background-color: #E14F76; border: 1px solid #007bff; color: white; padding: 10px 20px; text-decoration: none;">Click Here</a>
   <p>Thank you for choosing TuniWork!</p>
