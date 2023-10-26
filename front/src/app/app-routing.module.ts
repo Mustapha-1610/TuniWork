03b6934240986dd8d87fb29cf3925eaa9444c91e
Mustapha Talbi <mustapha.talbi2002@gmail.com>
2023-10-26 20:23:00 +0100

    changes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FreelancerlayoutComponent } from './layout/freelancerlayout/freelancerlayout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'freelancer',
+    component: FreelancerlayoutComponent,
+    children: [
+      {
+        path: 'signup',
+        loadChildren: () =>
+          import('./views/freelancer/signup/signup-routing.module').then(
+            (m) => m.SignupRoutingModule
+          ),
+      },
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./views/freelancer/login/login-routing.module').then(
+            (m) => m.LoginRoutingModule
+          ),
+      },
+      {
+        path: 'profile',
+        loadChildren: () =>
+          import('./views/freelancer/profile/profile-routing.module').then(
+            (m) => m.ProfileRoutingModule
+          ),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ front/src/app/layout/freelancerlayout/freelancerlayout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-freelancerlayout',
+  templateUrl: './freelancerlayout.component.html',
+  styleUrls: ['./freelancerlayout.component.css']
+})
+export class FreelancerlayoutComponent {
+
+}
