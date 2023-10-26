12c6f0d9f485ca88a0d8d20215ae24e44b6750d5
NINETAILEDKYUBI <noureddinebentaher20@gmail.com>
2023-10-26 18:07:47 +0100

    updated version noureddine

--- node/src/Admin/Router.ts
+++ node/src/Admin/Router.ts
@@ -3,9 +3,13 @@ import * as adminController from "./controller";
 
 
 
+
 const adminRouter = express.Router();
 
 adminRouter.post("/create", adminController.create);
+adminRouter.get("/get/:id",adminController.getAdminProfile);
+adminRouter.put("/update/:id",adminController.updateAdmin);
+adminRouter.delete("/delete/:id",adminController.deleteAdminAccount);
 
 
 export default adminRouter;
