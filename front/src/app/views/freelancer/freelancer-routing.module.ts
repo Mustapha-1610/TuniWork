7f33349eb440ae8bb444da7de979682c41d90fd7
Mustapha Talbi <mustapha.talbi2002@gmail.com>
2023-11-22 10:19:44 +0100

    front changes with best matches option filtering initialized

--- front/src/app/views/freelancer/freelancer-routing.module.ts
+++ front/src/app/views/freelancer/freelancer-routing.module.ts
@@ -5,12 +5,17 @@ import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { FindWorkPageComponent } from './find-work-page/find-work-page.component';
 import { CheckWorkOfferDetailsComponent } from './check-work-offer-details/check-work-offer-details.component';
+import { EditInfosPageComponent } from './edit-infos-page/edit-infos-page.component';
+import { PassResetPageComponent } from './pass-reset-page/pass-reset-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'browse', component: FindWorkPageComponent },
   { path: 'profile', component: ProfilePageComponent },
   { path: 'WorkOffer/:id', component: CheckWorkOfferDetailsComponent },
+  { path: 'browse', component: FindWorkPageComponent },
+  { path: 'editInfos', component: EditInfosPageComponent },
+  { path: 'editPassword', component: PassResetPageComponent },
 ];
 
 @NgModule({
