1a23e9d1503f946d2b0af0d50828f819e93f21bc
JIHED <119008292+JI8HED@users.noreply.github.com>
2023-12-14 21:53:21 +0100

    update front login multiauth
    -_-

--- front/src/app/views/Customer/profile-page/profile-page.component.ts
+++ front/src/app/views/Customer/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent {
+  companyData: any = this.cus.getCustomerInfos();
+
+  constructor(private cus: CustomerService) {}
+
+  disableAccount() {
+    this.cus.disableAccount().subscribe(
+      (response: any) => {
+        if (response.success) {
+          console.log(response.success);
+          this.cus.logout();
+        } else {
+          // Handle error response
+          console.error(response.error);
+        }
+      },
+      (error) => {
+        console.error('Error disabling account:', error);
+      }
+    );
+  }
 
 }
